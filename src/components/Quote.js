b00695f54a11b3bc1db39193622b22928c0a46ca
thatOneJon <83345498+ThatOneJon@users.noreply.github.com>
2023-01-25 22:14:46 +0100

    quote

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,10 +1,12 @@
 import React from "react"
+import { motion } from "framer-motion"
 
 
 export default function Quote(){
 
     const[quote, setQuote] = React.useState()
     const[changeQuote, setChangeQuote] = React.useState()
+    const[rotation, setRotation] = React.useState(0)
 
     let randomNum = () => {
         return Math.floor(Math.random() * 1640)
@@ -12,7 +14,9 @@ export default function Quote(){
 
 
     const handleClick = () => {
+        setRotation(rotation + 360)
         setChangeQuote(quote[randomNum()])
+
     }
 
     React.useEffect(() => {
@@ -27,15 +31,12 @@ export default function Quote(){
 
     }, [])
 
-
-console.log(changeQuote, quote)
-
     return(
-        <div style={{textAlign:"center",color:"black"}} className="flex flex-col justify-center align-middle">
+        <motion.div animate={{rotateY: rotation}} transition={{duration:1.4}} style={{textAlign:"center",color:"black"}} className=" bg-orange-300 flex flex-col justify-center align-middle shadow-2xl p-5">
            <h4 className="text-4xl underline mt-5">Have a random quote!</h4>
            <h5 className="text-4xl mb-10 mt-16">{changeQuote && changeQuote.text}</h5>
            <p className="text-2xl">- {changeQuote && changeQuote.author}</p>
            <button className="mt-14 hover:underline bg-orange-600 p-5 rounded hover:border-solid hover:border-2 hover:border-slate-700 hover:shadow-xl" onClick={handleClick}>Need another ?</button>
-        </div>
+        </motion.div>
     )
 }
