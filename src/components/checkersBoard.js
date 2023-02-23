d624d462a9a1c2d345bcf9f698eebcac096e1d48
thatOneJon <83345498+ThatOneJon@users.noreply.github.com>
2023-02-23 22:42:23 +0100

    checkers

---
+++ src/components/checkersBoard.js
@@ -0,0 +1,96 @@
+import React from "react"
+
+
+
+
+export default function CheckersBoard(){
+    return(
+        <div className=" mx-auto w-3/4 h-screen mt-5 flex justify-between">
+            <div className="grid grid-rows-8  border border-xl border-slate-900 w-4/5 h-5/6 rounded p-1">
+                <div className="grid grid-cols-8">
+                    <div className="">1</div>
+                    <div className="bg-slate-900 rounded">1</div>
+                    <div>1</div>
+                    <div className="bg-slate-900 rounded">1</div>
+                    <div>1</div>
+                    <div className="bg-slate-900 rounded">1</div>
+                    <div>1</div>
+                    <div className="bg-slate-900 rounded">1</div>
+                </div>
+                <div className="grid grid-cols-8">
+                    <div className="bg-slate-900 rounded">1</div>
+                    <div>1</div>
+                    <div className="bg-slate-900 rounded">1</div>
+                    <div>1</div>
+                    <div className="bg-slate-900 rounded">1</div>
+                    <div>1</div>
+                    <div className="bg-slate-900 rounded">1</div>
+                    <div>1</div>
+                </div>
+                <div className="grid grid-cols-8">
+                    <div>1</div>
+                    <div className="bg-slate-900 rounded">1</div>
+                    <div>1</div>
+                    <div className="bg-slate-900 rounded">1</div>
+                    <div>1</div>
+                    <div className="bg-slate-900 rounded">1</div>
+                    <div>1</div>
+                    <div className="bg-slate-900 rounded">1</div>
+                </div>
+                <div className="grid grid-cols-8">
+                    <div className="bg-slate-900 rounded">1</div>
+                    <div>1</div>
+                    <div className="bg-slate-900 rounded">1</div>
+                    <div>1</div>
+                    <div className="bg-slate-900 rounded">1</div>
+                    <div>1</div>
+                    <div className="bg-slate-900 rounded">1</div>
+                    <div>1</div>
+                </div>
+                <div className="grid grid-cols-8">
+                    <div>1</div>
+                    <div className="bg-slate-900 rounded">1</div>
+                    <div>1</div>
+                    <div className="bg-slate-900 rounded">1</div>
+                    <div>1</div>
+                    <div className="bg-slate-900 rounded">1</div>
+                    <div>1</div>
+                    <div className="bg-slate-900 rounded">1</div>
+                </div>
+                <div className="grid grid-cols-8">
+                    <div className="bg-slate-900 rounded">1</div>
+                    <div>1</div>
+                    <div className="bg-slate-900 rounded">1</div>
+                    <div>1</div>
+                    <div className="bg-slate-900 rounded">1</div>
+                    <div>1</div>
+                    <div className="bg-slate-900 rounded">1</div>
+                    <div>1</div>
+                </div>
+                <div className="grid grid-cols-8">
+                    <div>1</div>
+                    <div className="bg-slate-900 rounded">1</div>
+                    <div>1</div>
+                    <div className="bg-slate-900 rounded">1</div>
+                    <div>1</div>
+                    <div className="bg-slate-900 rounded">1</div>
+                    <div>1</div>
+                    <div className="bg-slate-900 rounded">1</div>
+                </div>
+                <div className="grid grid-cols-8">
+                    <div className="bg-slate-900 rounded">1</div>
+                    <div>1</div>
+                    <div className="bg-slate-900 rounded">1</div>
+                    <div>1</div>
+                    <div className="bg-slate-900 rounded">1</div>
+                    <div>1</div>
+                    <div className="bg-slate-900 rounded">1</div>
+                    <div>1</div>
+                </div>
+            </div>
+            <h1 className="text-3xl font-bold">Points</h1>
+        </div>
+    )
+
+
+}
---
+++ src/pages/checkers.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Layout from "../components/layout"
+import CheckersBoard from "../components/checkersBoard"
+
+export default function Checkers(){
+    return(
+        <Layout>
+            <CheckersBoard />
+        </Layout>
+    )
+}
